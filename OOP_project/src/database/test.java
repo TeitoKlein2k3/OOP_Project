8758b5b59b8234b3f78d328c558442835f4b6530
quang2552003 <135411308+quang2552003@users.noreply.github.com>
2023-06-06 13:41:41 +0700

    Add files via upload

---
+++ OOP_project/src/database/test.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author tth
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        ConnectDB c = new ConnectDB();
+    }
+}
